ca98653d2aad8c5739c0354afca12a36b5533713
DuanVM <duan@guide.inc>
2024-02-15 19:48:59 +0900

    update access mdx

---
+++ app/route.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+
+const postsDirectory = path.join(process.cwd(), "lib");
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const lang = searchParams.get("lang");
+  const postType = searchParams.get("postType");
+  const id = searchParams.get("id");
+  const fullPath = path.join(postsDirectory, `${lang}/${postType}/${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  return NextResponse.json({ result: { content: fileContents } });
+}
